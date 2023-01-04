57d881952b8100e06399b5de3d1315c8c06b2677
Fagotto <joffrey.fagotto4@laposte.net>
2023-01-04 19:53:13 +0100

    Service for Note

---
+++ src/main/java/com/projet9/Note/Domain/NoteDTO.java
@@ -0,0 +1,15 @@
+//package com.projet9.Note.Domain;
+//
+//import org.springframework.data.annotation.Id;
+//
+//import java.util.Date;
+//
+//public class NoteDTO {
+//
+//    @Id
+//    private long idNote;
+//    private Date messageDate;
+//    private long patId;
+//
+//    private String notes;
+//}
