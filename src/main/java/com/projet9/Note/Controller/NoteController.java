f04b42233bd9ca613d2571cdf65e78d0282622b0
Fagotto <joffrey.fagotto4@laposte.net>
2023-02-02 14:24:53 +0100

    clean code

--- src/main/java/com/projet9/Note/Controller/NoteController.java
+++ src/main/java/com/projet9/Note/Controller/NoteController.java
@@ -2,11 +2,9 @@ package com.projet9.Note.Controller;
 
 import com.projet9.Note.Domain.Note;
 import com.projet9.Note.Service.NoteService;
-import com.projet9.Note.Service.SequenceGeneratorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
